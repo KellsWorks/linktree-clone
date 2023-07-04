7ee6fbcb8be98382ecce08b855c24681901594e1
kellworks <kchidothi@outlook.com>
2023-07-04 12:59:35 +0200

    frontend: changed to typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl bg-green-500">
+        MERN Srack
+      </div>
+    </div>
+  );
+}
+
+export default App;
